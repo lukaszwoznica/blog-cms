63e4a3fd9f274350a03624709539b0985eb92a44
lukaszwoznica <lukaszlwoznica@gmail.com>
2019-08-29 20:28:49 +0200

    Add class for sending emails

--- App/Config.php
+++ App/Config.php
@@ -25,4 +25,13 @@ class Config
      * Secret key for hashing
      */
     const SECRET_KEY = '3xRKxebnuVfQhoMMZfyZbv9XU4VaICVP';
+
+    /*
+     * PHPMailer settings
+     */
+    const SMTP_HOST = '';
+    const SMTP_USERNAME = '';
+    const SMTP_PASSWORD = '';
+    const SENDER_ADDRESS = '';
+    const SENDER_NAME = '';
 }
