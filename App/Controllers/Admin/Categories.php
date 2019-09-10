1f5ab47aef487dfc038102a343214dbf5ae02f52
lukaszwoznica <lukaszlwoznica@gmail.com>
2019-09-10 12:54:26 +0200

    Complete CRUD operations for categories

---
+++ App/Controllers/Admin/Categories.php
@@ -0,0 +1,103 @@
+<?php
+
+
+namespace App\Controllers\Admin;
+
+
+use App\Flash;
+use App\Models\Category;
+use Core\View;
+
+class Categories extends Admin
+{
+    private $id;
+
+    public function __construct(array $route_params)
+    {
+        parent::__construct($route_params);
+        $this->id = $route_params['id'] ?? null;
+    }
+
+    public function indexAction(): void
+    {
+        $categories = Category::getAllCategories();
+
+        View::renderTemplate('Admin/Categories/index.html', [
+            'categories' => $categories
+        ]);
+    }
+
+    public function newAction(): void
+    {
+        View::renderTemplate('Admin/Categories/new.html');
+    }
+
+    public function createAction(): void
+    {
+        $category = new Category($_POST);
+
+        if ($category->saveToDatabase()) {
+            Flash::addMessage('Category has been successfully added', Flash::SUCCESS);
+            $this->redirectTo('/admin/categories');
+        } else {
+            View::renderTemplate('Admin/Categories/new.html', [
+                'category' => $category
+            ]);
+        }
+    }
+
+    public function destroyAction(): void
+    {
+        $category = $this->getCategoryById($this->id);
+
+        if($category){
+            if ($category->delete()) {
+                Flash::addMessage('Category has been successfully deleted', Flash::SUCCESS);
+            } else {
+                Flash::addMessage('An error occurred while deleting the category', Flash::ERROR);
+            }
+        } else {
+            Flash::addMessage('Category with given id does not exist', Flash::WARNING);
+        }
+
+        $this->redirectTo('/admin/categories');
+    }
+
+    public function editAction(): void
+    {
+        $category = $this->getCategoryById($this->id);
+
+        if ($category){
+            View::renderTemplate('Admin/Categories/edit.html', [
+                'category' => $category
+            ]);
+        } else {
+            Flash::addMessage('Category with given id does not exist', Flash::WARNING);
+            $this->redirectTo('/admin/categories');
+        }
+
+    }
+
+    public function updateAction(): void
+    {
+        $category = $this->getCategoryById($this->id);
+
+        if ($category && $_SERVER['REQUEST_METHOD'] == 'POST') {
+            if ($category->update($_POST)) {
+                Flash::addMessage('Category has been successfully updated', Flash::SUCCESS);
+                $this->redirectTo('/admin/categories');
+            } else {
+                View::renderTemplate('Admin/Categories/edit.html', [
+                    'category' => $category
+                ]);
+            }
+        } else {
+            $this->redirectTo('/admin/categories');
+        }
+    }
+
+    private function getCategoryById(int $id): ?Category
+    {
+        return Category::findByID($id);
+    }
+}
---
+++ App/Models/Category.php
@@ -0,0 +1,143 @@
+<?php
+
+
+namespace App\Models;
+
+use Core\Model;
+use PDO;
+
+class Category extends Model
+{
+    private $id;
+    private $name;
+    private $description;
+    private $validation_errors = [];
+
+    public function __construct(array $category_data = [])
+    {
+        foreach ($category_data as $key => $value) {
+            $this->$key = $value;
+        }
+    }
+
+    public function saveToDatabase(): bool
+    {
+        $this->validate();
+
+        if (empty($this->validation_errors)){
+            $db = self::getDatabase();
+            $sql = 'INSERT INTO categories (name, description)
+                VALUES (:name, :description)';
+
+            $stmt = $db->prepare($sql);
+            $stmt->bindValue(':name', trim($this->name), PDO::PARAM_STR);
+            $stmt->bindValue('description', $this->description, PDO::PARAM_STR);
+
+            return $stmt->execute();
+        }
+
+        return false;
+    }
+
+    public function validate(): void
+    {
+        // Name
+        if (strlen(trim($this->name)) < 2 || strlen(trim($this->name)) > 100) {
+            $this->validation_errors[] = "Category name must be between 2 and 100 characters";
+        }
+
+        // Description
+        if (strlen($this->description) > 255) {
+            $this->validation_errors[] = 'Category description cannot be longer than 255 characters';
+        }
+    }
+
+    public static function getAllCategories(): array
+    {
+        $db = static::getDatabase();
+        $sql = 'SELECT * FROM categories';
+
+        $stmt = $db->query($sql);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
+        $result = $stmt->fetchAll();
+
+        return $result;
+    }
+
+    public static function findByID(int $category_id): ?Category
+    {
+        $db = static::getDatabase();
+        $sql = 'SELECT * FROM categories WHERE id = :category_id';
+
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(":category_id", $category_id, PDO::PARAM_INT);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
+        $stmt->execute();
+
+        $result = $stmt->fetch();
+        if (!$result)
+            return null;
+        return $result;
+    }
+
+    public function delete(): bool
+    {
+        $db = static::getDatabase();
+        $sql = 'DELETE FROM categories WHERE id = :id';
+
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':id', $this->id);
+
+        return $stmt->execute();
+    }
+
+    public function update(array $category_data): bool
+    {
+        $this->name = trim($category_data['name']);
+        $this->description = $category_data['description'];
+
+        $this->validate();
+
+        if (empty($this->validation_errors)) {
+            $db = static::getDatabase();
+            $sql = 'UPDATE categories
+                    SET name = :name,
+                    description = :description                    
+                    WHERE id = :id';
+
+            $stmt = $db->prepare($sql);
+            $stmt->bindValue(':name', $this->name, PDO::PARAM_STR);
+            $stmt->bindValue(':description', $this->description, PDO::PARAM_STR);
+            $stmt->bindValue(':id', $this->id, PDO::PARAM_INT);
+
+            return $stmt->execute();
+        }
+
+        return false;
+    }
+
+    /*
+     * Getters
+     */
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function getValidationErrors(): array
+    {
+        return $this->validation_errors;
+    }
+
+}
