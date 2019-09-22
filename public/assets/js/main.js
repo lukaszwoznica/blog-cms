43ade687192f815260e025d481aa9fc9dd7cef46
lukaszwoznica <lukaszlwoznica@gmail.com>
2019-09-22 16:45:38 +0200

    Styles for base admin panel template

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
-    $('.sidenav').sidenav();
+    $('.sidenav').sidenav({
+        draggable: true
+    });
+
+    $(".dropdown-trigger").dropdown({
+        coverTrigger: false
+    });
 
     $('.alert > button').on('click', function(){
         $(this).closest('div.alert').fadeOut('slow');
